23ef7f188b96e661ebb4818135e68f04481dbaeb
mesfik <fikreamesfin@gmail.com>
2023-06-22 02:32:56 +0300

    sql dev

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,7 @@
+-- a script that prepares a MySQL server
+-- hbnb_dev should have all privileges on the database hbnb_dev_db
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+USE hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
