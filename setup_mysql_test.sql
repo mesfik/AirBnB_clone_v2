6c8da14da26c351862dca5878a0f171232e2af14
mesfik <fikreamesfin@gmail.com>
2023-06-22 02:41:56 +0300

    mysql test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,7 @@
+-- a script that prepares a MySQL server
+-- hbnb_dev should have all privileges on the database hbnb_test_db
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+USE hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
