f4efdef8bf76d85ba57d682f7663b92cb3bf0b27
mesfik <fikreamesfin@gmail.com>
2023-06-24 22:36:36 +0300

    db storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -70,7 +70,3 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
-
-    def close(self):
-        """call remove() method on the private session attribute"""
-        self.__session.remove()
