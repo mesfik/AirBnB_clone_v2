22f6adc944b591c66702780ce83988af85f903f9
mesfik <fikreamesfin@gmail.com>
2023-06-24 17:42:52 +0300

    city

--- models/city.py
+++ models/city.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), nullable=False, foreign_key('states.id'))
+
+    if models.storage_type == 'db':
+        state = relationship('State', back_populates='cities')
+        places = relationship('Place', back_populates='city')
+    else:
+        name = ""
+        state_id = ""
--- models/state.py
+++ models/state.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+""" holds class State"""
+import models
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """ class state that is inherited from basemdel and base"""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    if models.storage_type == 'db':
+        cities = relationship("City", cascade="all, delete", backref="state")
+
+    else:
+        @property
+        def cities(self):
+            """getter for list of city instances related to the state"""
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+
+            return city_list
