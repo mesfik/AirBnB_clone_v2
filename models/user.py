98cc15266204f4802dc0814f54c8a923da9b2b3c
mesfik <fikreamesfin@gmail.com>
2023-06-24 23:35:56 +0300

    user

--- models/user.py
+++ models/user.py
@@ -9,13 +9,12 @@ import models
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     if models.storage_type == "db":
-        __tablename__ = 'users'
-
+        __tablename__ = "users"
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
-
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+       # places = relationship('Place', back_populates='user')
     else:
         email = ''
         password = ''
