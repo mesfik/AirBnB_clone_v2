28ea1f145405757e74389cac0f6d6dfe2f3dadee
mesfik <fikreamesfin@gmail.com>
2023-07-12 15:23:32 +0300

    web statics

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# setting up web server for web-static
+apt-get -y update
+apt-get -y install nginx
+mkdir -p /data/web_static/releases/test
+mkdir -p /data/web_static/shared
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -hR ubuntu:ubuntu /data/
+sed -i '/^server {/a\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+service nginx restart
