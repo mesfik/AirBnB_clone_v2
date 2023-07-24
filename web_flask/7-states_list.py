6ff6e8b4d2356b6a5f208a76bad8bfe713b412f3
mesfik <fikreamesfin@gmail.com>
2023-07-24 04:18:34 +0300

    pycodestyle

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,6 +11,7 @@ from operator import attrgetter
 
 app = Flask(__name__)
 
+
 @app.route('/states_list', strict_slashes=False)
 def state_list():
     """
@@ -20,10 +21,12 @@ def state_list():
     states = sorted(states, key=lambda k: k.name)
     return render_template('7-states_list.html', states=states)
 
+
 @app.teardown_appcontext
 def teardown(error):
     """ closing the sqlAlchemy"""
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
